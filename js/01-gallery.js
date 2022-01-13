93e5f5c052a9eb4d7374799df78dd7c10b6eed66
galinafrost <morozgalina13@gmail.com>
2022-01-13 17:22:14 +0200

    Оформлены задания 2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
 
-console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+const cardsImage = createImageMarcup();
+
+galleryContainerEl.addEventListener('click', onImageContainerClick);
+
+function createImageMarcup() {
+  const markup = galleryItems.map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+  }).join('');
+
+  galleryContainerEl.insertAdjacentHTML('afterbegin', markup)
+};
+
+function onImageContainerClick(event) {
+  window.addEventListener('keydown', onEscKeyPress);
+
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+  instance.show()
+
+  function onEscKeyPress(event) {
+    if (event.code === 'Escape') {
+      instance.close()
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+
+function createImageMarcup() {
+    const markup = galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    }).join('');
+
+    galleryContainerEl.insertAdjacentHTML('afterbegin', markup)
+};
+
+createImageMarcup();
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+    captionPosition: 'top',
+    navText: ['<', '>']
+});
+
